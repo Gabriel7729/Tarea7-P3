0da177614febaf5a813289c29550c1c22225110c
Gabriel7729 <Lusan7729@gmail.com>
2021-03-10 21:18:58 -0400

    Apartado de filtros de Datos Agregado y Correcion de errores del Registro

---
+++ Data/vacunados.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace __tarea7.Data
+{
+    public class vacunados
+    {
+        public long Id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public long telefono { get; set; }
+        public string fechaNacimiento { get; set; }
+        public string tipoVacuna { get; set; }
+        public string provincia { get; set; }
+
+        public string fecahDosis1 { get; set; }
+        public string fecahDosis2 { get; set; }
+
+        public String SignoZodiacal(DateTime fecha)
+        {
+            int mes = fecha.Month;
+            int dia = fecha.Day;
+
+            switch (mes)
+            {
+                case 1:
+                    return (dia < 21) ? "Capricornio" : "Acuario";
+                case 2:
+                    return (dia < 20) ? "Acuario" : "Piscis";
+                case 3:
+                    return (dia < 21) ? "Piscis" : "Aries";
+                case 4:
+                    return (dia < 20) ? "Aries" : "Tauro";
+                case 5:
+                    return (dia < 21) ? "Tauro" : "Géminis";
+                case 6:
+                    return (dia < 20) ? "Géminis" : "Cancer";
+                case 7:
+                    return (dia < 21) ? "Cancer" : "Leo";
+                case 8:
+                    return (dia < 20) ? "Leo" : "Virgo";
+                case 9:
+                    return (dia < 21) ? "Virgo" : "Libra";
+                case 10:
+                    return (dia < 22) ? "Libra" : "Escorpio";
+                case 11:
+                    return (dia < 23) ? "Escorpio" : "Sagitario";
+                case 12:
+                    return (dia < 19) ? "Sagitario" : "Capricornio";
+            }
+
+            throw new Exception("Error");
+        }
+    }
+}
---
+++ Data/vacunadosServices.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace __tarea7.Data
+{
+    public class vacunadosServices
+    {
+        #region Private members
+        private vacunadosDbContext dbContext;
+        #endregion
+
+        #region Constructor
+        public vacunadosServices(vacunadosDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        #endregion
+
+        #region Public methods
+        /// <summary>
+        /// This method returns the list of product
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<vacunados>> GetProductAsync()
+        {
+            return await dbContext.Product.ToListAsync();
+        }
+
+        /// <summary>
+        /// This method add a new product to the DbContext and saves it
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public async Task<vacunados> AddProductAsync(vacunados product)
+        {
+            try
+            {
+                dbContext.Product.Add(product);
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return product;
+        }
+
+        /// <summary>
+        /// This method update and existing product and saves the changes
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public async Task<vacunados> UpdateProductAsync(vacunados product)
+        {
+            try
+            {
+                var productExist = dbContext.Product.FirstOrDefault(p => p.Id == product.Id);
+                if (productExist != null)
+                {
+                    dbContext.Update(product);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return product;
+        }
+
+        /// <summary>
+        /// This method removes and existing product from the DbContext and saves it
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public async Task DeleteProductAsync(vacunados product)
+        {
+            try
+            {
+                dbContext.Product.Remove(product);
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
