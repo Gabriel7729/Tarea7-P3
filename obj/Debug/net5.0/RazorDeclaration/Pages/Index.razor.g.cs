7553b1bb2a65900ee19477503aca4f3fd767e159
Gabriel7729 <Lusan7729@gmail.com>
2021-03-11 08:46:42 -0400

    Apartado de filtros de Datos Agregado y Correcion de errores del Registro y Graficos añadidos.

--- obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,77 +13,77 @@ namespace __tarea7.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 1 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 2 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 3 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 4 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 5 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 6 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 7 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 8 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 9 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using __tarea7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 10 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using __tarea7.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\Pages\Index.razor"
+#line 2 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\Pages\Index.razor"
 using Data;
 
 #line default
@@ -98,7 +98,7 @@ using Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 130 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\Pages\Index.razor"
+#line 132 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\Pages\Index.razor"
         List<vacunados> Products = new List<vacunados>();
 
     protected override async Task OnInitializedAsync()
@@ -122,6 +122,7 @@ using Data;
     }
 
     vacunados UpdateProduct = new vacunados();
+
     private void SetProductForUpdate(vacunados product)
     {
         UpdateProduct = product;
--- obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace __tarea7.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 1 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 2 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 3 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 4 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 5 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 6 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 7 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 8 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 9 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using __tarea7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\LUIS ANTONIO DE AZA\source\repos\__tarea7\__tarea7\_Imports.razor"
+#line 10 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
 using __tarea7.Shared;
 
 #line default
