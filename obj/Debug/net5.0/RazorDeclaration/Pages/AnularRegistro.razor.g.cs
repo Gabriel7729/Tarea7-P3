232c833e6bb012b377da5b5206844ffb22ab08b0
Gabriel7729 <Lusan7729@gmail.com>
2021-03-11 17:49:43 -0400

    Pequeños errores corregidos y un uevo registro añadido.

--- obj/Debug/net5.0/RazorDeclaration/Pages/AnularRegistro.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/AnularRegistro.razor.g.cs
@@ -13,84 +13,84 @@ namespace __tarea7.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 2 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 3 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 4 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 5 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 6 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 7 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 8 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 9 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using __tarea7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 10 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using __tarea7.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\Pages\AnularRegistro.razor"
+#line 2 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Pages\AnularRegistro.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\Pages\AnularRegistro.razor"
+#line 3 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Pages\AnularRegistro.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\Pages\AnularRegistro.razor"
+#line 4 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Pages\AnularRegistro.razor"
 using Data;
 
 #line default
@@ -105,7 +105,7 @@ using Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 53 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\Pages\AnularRegistro.razor"
+#line 53 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Pages\AnularRegistro.razor"
        
     public class Persona
     {
--- obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace __tarea7.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 1 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 2 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 3 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 4 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 5 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 6 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 7 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 8 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 9 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using __tarea7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\_Imports.razor"
+#line 10 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
 using __tarea7.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using __tarea7.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "C:\Users\LUIS ANTONIO DE AZA\Desktop\clon\Tarea7-P3\Shared\NavMenu.razor"
+#line 28 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
