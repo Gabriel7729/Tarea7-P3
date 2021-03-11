2494f06f3d5d00c8d7198862446971f526de893e
Gabriel7729 <Lusan7729@gmail.com>
2021-03-11 08:55:13 -0400

    Errores corregidos

--- obj/Debug/net5.0/RazorDeclaration/Pages/RegistrarVacuna.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/RegistrarVacuna.razor.g.cs
@@ -13,84 +13,84 @@ namespace __tarea7.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
+#line 2 "C:\Users\Lusan\Desktop\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
+#line 3 "C:\Users\Lusan\Desktop\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
+#line 4 "C:\Users\Lusan\Desktop\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
+#line 5 "C:\Users\Lusan\Desktop\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
+#line 6 "C:\Users\Lusan\Desktop\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
+#line 7 "C:\Users\Lusan\Desktop\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
+#line 8 "C:\Users\Lusan\Desktop\Tarea7-P3\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
+#line 9 "C:\Users\Lusan\Desktop\Tarea7-P3\_Imports.razor"
 using __tarea7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\_Imports.razor"
+#line 10 "C:\Users\Lusan\Desktop\Tarea7-P3\_Imports.razor"
 using __tarea7.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\Pages\RegistrarVacuna.razor"
+#line 2 "C:\Users\Lusan\Desktop\Tarea7-P3\Pages\RegistrarVacuna.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\Pages\RegistrarVacuna.razor"
+#line 3 "C:\Users\Lusan\Desktop\Tarea7-P3\Pages\RegistrarVacuna.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\Pages\RegistrarVacuna.razor"
+#line 4 "C:\Users\Lusan\Desktop\Tarea7-P3\Pages\RegistrarVacuna.razor"
 using Data;
 
 #line default
@@ -105,7 +105,7 @@ using Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 138 "C:\Users\Lusan\Downloads\Tarea7-P3-Luis\Pages\RegistrarVacuna.razor"
+#line 138 "C:\Users\Lusan\Desktop\Tarea7-P3\Pages\RegistrarVacuna.razor"
        
 
     public class Persona
