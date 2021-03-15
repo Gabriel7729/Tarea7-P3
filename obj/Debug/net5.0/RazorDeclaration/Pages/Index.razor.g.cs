10df2018c8f9d05df813084f718fd9f72ec21f5d
Anthony-Valdez <anthony_valdez012@hotmail.com>
2021-03-15 00:40:14 -0400

    Configuracion

--- obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,77 +13,77 @@ namespace __tarea7.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 1 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 2 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 3 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 4 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 5 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 6 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 7 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 8 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 9 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using __tarea7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 10 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using __tarea7.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 1 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Pages\Index.razor"
+#line 1 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\Pages\Index.razor"
 using Data;
 
 #line default
@@ -97,7 +97,7 @@ using Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 131 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Pages\Index.razor"
+#line 131 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\Pages\Index.razor"
         List<vacunados> Products = new List<vacunados>();
 
     protected override async Task OnInitializedAsync()
--- obj/Debug/net5.0/RazorDeclaration/Pages/RegistrarVacuna.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/RegistrarVacuna.razor.g.cs
@@ -13,84 +13,84 @@ namespace __tarea7.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 2 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 3 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 4 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 5 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 6 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 7 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 8 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 9 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using __tarea7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\_Imports.razor"
+#line 10 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\_Imports.razor"
 using __tarea7.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Pages\RegistrarVacuna.razor"
+#line 2 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\Pages\RegistrarVacuna.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Pages\RegistrarVacuna.razor"
+#line 3 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\Pages\RegistrarVacuna.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Pages\RegistrarVacuna.razor"
+#line 4 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\Pages\RegistrarVacuna.razor"
 using Data;
 
 #line default
@@ -105,7 +105,7 @@ using Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 141 "C:\Users\Lusan\Documents\MEGAsync Downloads\Tarea7-P3\Pages\RegistrarVacuna.razor"
+#line 156 "c:\Users\a\Downloads\ITLA-seaon 6\Prog 3\T7\Tarea7-P3\Pages\RegistrarVacuna.razor"
        
 
     public class Persona
@@ -159,6 +159,11 @@ using Data;
     String provincia = "-- Sin seleccionar --";
     DateTime fechaDosis1;
     long telefono;
+    
+    int VacA;
+    int VacB;
+    int  VacC;
+
 
     //Registrados
     String FechaD1Registrado;
